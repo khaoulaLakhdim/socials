0c25a26f6792686a2dd12ce187dacc2a2a95daf8
unknown <nourlaajane03@gmail.com>
2025-06-04 16:18:12 +0100

    database models added

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from contextlib import asynccontextmanager
-from database import db_init
-from models import Test
-
+from database import db_init, get_session
+from models import User, Post,user_post
+from sqlmodel import Session
+from pydantic import BaseModel
 @asynccontextmanager
 async def lifespan(app : FastAPI):
     db_init()
     yield
 
+class UserBase(BaseModel):
+    name: str
+    email: str
+
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
-async def home():
-    message = "hi baby"
-    return message
+@app.post("/users/")
+def add_user(user: UserBase,session: Session = Depends(get_session)):
+    #we need to check if the email already exist
+    to_check = session.get(User,user.email)
+    if not to_check:
+        toadd = User(name = user.name, email = user.email)
+        session.add(toadd)
+        session.commit()
+        session.refresh(toadd)
+        return toadd
+    else:
+        raise HTTPException(status_code=409, detail="email already exists")
 
